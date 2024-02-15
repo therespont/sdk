8bae4017a96f39d1cf053c57f52bff779317e506
ilhamm02 <ilhamaulana24@gmail.com>
2024-02-15 21:46:02 +0700

    Fix some throw error

---
+++ index.js
@@ -0,0 +1,5 @@
+import Connect from "./lib/mjs/src/Connection.js";
+import Listen from "./lib/mjs/src/Listeners.js";
+import Interactions from "./lib/mjs/src/Interactions.js";
+
+export { Connect, Listen, Interactions };
